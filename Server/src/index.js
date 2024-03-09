77258ee3a357d09c5a3e9f057dcb93beabcf8252
Jorge Nahuel Beja Rosa <jorgebejarosa@gmail.com>
2024-03-08 21:59:36 -0300

    [BUILD] Install MongoDB and Mongoose

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
 const port = process.env.PORT || 3001
@@ -6,6 +7,10 @@ const port = process.env.PORT || 3001
 app.get('/', (req, res) => {
   res.send('Hello, world!')
 })
+
+// MongoDB connection
+
+mongoose.connect()
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`)
