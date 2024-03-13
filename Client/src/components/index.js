bab3225e1e47966283fba65cd9825f8fa43c06bc
Jorge Nahuel Beja Rosa <jorgebejarosa@gmail.com>
2024-03-13 08:17:34 -0300

    [ADD] Index Components

---
+++ Client/src/components/index.js
@@ -0,0 +1,12 @@
+import Footer from './Footer/Footer'
+import NavBar from './Navbar/Navbar'
+import BooksCards from './BooksCards/BooksCards'
+import BookCard from './BookCard/BookCard'
+import BooksForm from './BooksForm/BooksForm'
+import FormField from './FormField/FormField'
+import ButtonCard from './ButtonCard/ButtonCard'
+import FavoriteButton from './FavoriteButton/FavoriteButton'
+import FavoriteBooksCards from './FavoriteBooksCards/FavoriteBooksCards'
+import SearchBar from './SearchBar/SearchBar'
+
+export { Footer, SearchBar, NavBar, BooksCards, BookCard, BooksForm, FormField, ButtonCard, FavoriteButton, FavoriteBooksCards }
