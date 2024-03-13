5538450a5a5f41ca586770112159c06fcb282c72
Jorge Nahuel Beja Rosa <jorgebejarosa@gmail.com>
2024-03-13 08:10:58 -0300

    [BUILD] Router Main

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { RouterProvider } from 'react-router-dom'
+import { router } from './router/index'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 )
