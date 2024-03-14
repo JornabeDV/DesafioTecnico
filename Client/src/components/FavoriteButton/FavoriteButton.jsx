9436b98bc7046d0755e47af05326d471c788f0dd
Jorge Nahuel Beja Rosa <jorgebejarosa@gmail.com>
2024-03-14 10:41:46 -0300

    [FIX] Change React Icon

--- Client/src/components/FavoriteButton/FavoriteButton.jsx
+++ Client/src/components/FavoriteButton/FavoriteButton.jsx
@@ -1,9 +1,9 @@
-import { FaStar, FaRegStar } from 'react-icons/fa'
+import { BsStar, BsStarFill } from 'react-icons/bs'
 
 const FavoriteButton = ({ isFavorite, onClick }) => {
   return (
-    <button onClick={onClick} className='text-2xl text-yellow-400 focus:outline-none'>
-      {isFavorite ? <FaStar /> : <FaRegStar />}
+    <button onClick={onClick}>
+      {isFavorite ? <BsStarFill color='#80CC28' size={20} /> : <BsStar color='#80CC28' size={20} />}
     </button>
   )
 }
