a8531714fbfefd1b37edea10aea8567b0ec8c71e
Jorge Nahuel Beja Rosa <jorgebejarosa@gmail.com>
2024-03-14 10:33:27 -0300

    [ADD] Try-Catch in the Actions and ToggleFavorite Action

--- Client/src/app/actions.js
+++ Client/src/app/actions.js
@@ -2,25 +2,64 @@ import { createAsyncThunk } from '@reduxjs/toolkit'
 import axios from 'axios'
 
 export const fetchBooks = createAsyncThunk('books/fetchBooks', async () => {
-  const response = await axios.get('http://localhost:3001/api/books')
-  return response.data
+  try {
+    const response = await axios.get('http://localhost:3001/api/books')
+    return response.data
+  } catch (error) {
+    console.error('Error fetching books:', error)
+    throw error
+  }
 })
+
 export const searchBooksByTerm = createAsyncThunk('books/searchBooksByTerm', async (searchTerm) => {
-  const response = await axios.get(`http://localhost:3001/api/books?search=${searchTerm}`)
-  return response.data
+  try {
+    const response = await axios.get(`http://localhost:3001/api/books?search=${searchTerm}`)
+    return response.data
+  } catch (error) {
+    console.error('Error searching books by term:', error)
+    throw error
+  }
 })
 
 export const addBook = createAsyncThunk('books/addBook', async (book) => {
-  const response = await axios.post('http://localhost:3001/api/books', book)
-  return response.data
+  try {
+    const response = await axios.post('http://localhost:3001/api/books', book)
+    return response.data
+  } catch (error) {
+    console.error('Error adding book:', error)
+    throw error
+  }
 })
 
 export const editBook = createAsyncThunk('books/editBook', async ({ id, book }) => {
-  const response = await axios.put(`http://localhost:3001/api/books/${id}`, book)
-  return { id, book: response.data }
+  try {
+    const response = await axios.put(`http://localhost:3001/api/books/${id}`, book)
+    return { id, book: response.data }
+  } catch (error) {
+    console.error('Error editing book:', error)
+    throw error
+  }
 })
 
 export const deleteBook = createAsyncThunk('books/deleteBook', async (_id) => {
-  await axios.delete(`http://localhost:3001/api/books/${_id}`)
-  return _id
+  try {
+    await axios.delete(`http://localhost:3001/api/books/${_id}`)
+    return _id
+  } catch (error) {
+    console.error('Error deleting book:', error)
+    throw error
+  }
 })
+
+export const toggleFavorite = createAsyncThunk(
+  'books/toggleFavorite',
+  async ({ _id, isFavorite }) => {
+    try {
+      const response = await axios.put(`http://localhost:3001/api/books/${_id}`, { favorite: isFavorite })
+      return { _id, book: response.data }
+    } catch (error) {
+      console.error('Error toggling favorite:', error)
+      throw error
+    }
+  }
+)
