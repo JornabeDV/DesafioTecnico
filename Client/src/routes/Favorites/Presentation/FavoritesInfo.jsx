5ed4f7a4c676e0721f3387c5527425be58bbb0d7
Jorge Nahuel Beja Rosa <jorgebejarosa@gmail.com>
2024-03-14 10:45:37 -0300

    [FIX] Change SetFavorite to ToggleFavorite

--- Client/src/routes/Favorites/Presentation/FavoritesInfo.jsx
+++ Client/src/routes/Favorites/Presentation/FavoritesInfo.jsx
@@ -1,21 +1,19 @@
 import { useDispatch, useSelector } from 'react-redux'
-import { setFavorite } from '../../../features/books/bookSlice'
 import FavoriteBooksCards from '../../../components/FavoriteBooksCards/FavoriteBooksCards'
+import { toggleFavorite } from '../../../app/actions'
 
 const FavoritesInfo = () => {
   const dispatch = useDispatch()
   const books = useSelector((state) => state.books.books)
-  console.log(books)
   if (!Array.isArray(books.data)) {
-    return <div>Cargando libros...</div>
+    return <div>Loading books...</div>
+  }
+  const favoriteBooks = books.data.filter(book => book.favorite === true)
+
+  const handleFavoriteClick = (_id, isFavorite) => {
+    dispatch(toggleFavorite({ id: _id, currentFavorite: isFavorite }))
   }
 
-  const favoriteBooks = books.data.filter(book => book.favorite === true)
-  console.log(favoriteBooks)
-  const handleFavoriteClick = (_id, isFavorite) => {
-    dispatch(setFavorite({ _id, isFavorite }))
-  }
-  console.log(handleFavoriteClick)
   return (
     <div className='p-5'>
       <h1 className='text-[50px] lg:text-[90px] font-bold mb-4 p-5 text-center text-[#80CC28] font-josefin'>FAVORITES</h1>
