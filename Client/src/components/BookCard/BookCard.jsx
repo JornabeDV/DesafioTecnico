efe6cabb5df42c6ee26e2f3b874511b3b8e63a10
Jorge Nahuel Beja Rosa <jorgebejarosa@gmail.com>
2024-03-14 10:35:53 -0300

    [FIX] Change SetFavorite to ToggleFavorite and change  handleFavorite function.

--- Client/src/components/BookCard/BookCard.jsx
+++ Client/src/components/BookCard/BookCard.jsx
@@ -1,22 +1,24 @@
-import React from 'react'
+import { useState } from 'react'
 import { useDispatch } from 'react-redux'
 import swal from 'sweetalert'
 import ButtonCard from '../ButtonCard/ButtonCard'
 import FavoriteButton from '../FavoriteButton/FavoriteButton'
+import { toggleFavorite } from '../../app/actions'
 import { Link } from 'react-router-dom'
-import { setFavorite } from '../../features/books/bookSlice'
 
 const BookCard = ({ book, onDelete }) => {
   const dispatch = useDispatch()
+  const [isFavorite, setIsFavorite] = useState(book.favorite)
 
   const showAlert = (message) => {
     swal(message)
   }
 
   const handleFavorite = () => {
-    const newFavoriteState = !book.favorite
-    swal(newFavoriteState ? 'Book marked as favorite' : 'Book removed from favorites')
-    dispatch(setFavorite({ _id: book._id, isFavorite: newFavoriteState }))
+    const newFavoriteState = !isFavorite
+    setIsFavorite(newFavoriteState)
+    dispatch(toggleFavorite({ _id: book._id, isFavorite: newFavoriteState }))
+    showAlert(newFavoriteState ? 'Book marked as favorite' : 'Book removed from favorites')
   }
 
   const handleDelete = () => {
@@ -25,8 +27,8 @@ const BookCard = ({ book, onDelete }) => {
   }
 
   return (
-    <div className='bg-white border border-[#B2FA5B] rounded-lg shadow-md overflow-hidden flex flex-col items-center relative p-2'>
-      <FavoriteButton isFavorite={book.favorite} onClick={handleFavorite} className='ab top-0 right-0 m-2' />
+    <div className='bg-white border border-gray-200 rounded-lg shadow-md overflow-hidden flex flex-col items-center relative p-2'>
+      <FavoriteButton isFavorite={isFavorite} onClick={handleFavorite} className='relatives top-0 right-0 m-2' />
       <div className='p-4 text-center'>
         <h3 className='text-xl font-bold mb-2'>{book.title}</h3>
         <p className='text-gray-600'>Author: {book.author}</p>
@@ -34,7 +36,7 @@ const BookCard = ({ book, onDelete }) => {
         <p className='text-gray-600'>Genre: {book.genre}</p>
       </div>
       <div className='p-4'>
-        <ButtonCard onClick={handleDelete} color='red'>Eliminar</ButtonCard>
+        <ButtonCard onClick={handleDelete}>Delete</ButtonCard>
         <Link to={`/Form/${book._id}`}>
           <ButtonCard>Editar</ButtonCard>
         </Link>
